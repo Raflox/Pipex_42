e1fb76ff382c836e31618153c643dad3b76f3143
Raflox <rafaeldgamasilva@gmail.com>
2023-03-20 11:19:32 +0000

    testing

---
+++ pipex.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafilipe <rafilipe@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 15:48:43 by rafilipe          #+#    #+#             */
+/*   Updated: 2023/03/20 10:57:18 by rafilipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include <sys/wait.h>
+
+void    pipex(int f1, int f2, char *cmd1, char *cmd2, char **envp);
+void	child_process(char **argv, int *end, char **envp);
+
+
+// Check if FD's are closed --> lsof -c a.out --> infinite loop
+#endif
